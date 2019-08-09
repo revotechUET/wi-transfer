779c4b641478208800fb3db9475e6d5e17b9753c
BuiHoang <bdhoang.ht@gmail.com>
2019-08-09 15:58:24 +0700

    fix

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const config = require('config');
+let crypto = require('crypto');
+let serverId = getRandomHash();
+
+function getRandomHash() {
+	const current_date = (new Date()).valueOf().toString();
+	const random = Math.random().toString();
+	return (crypto.createHash('sha1').update(current_date + random).digest('hex'));
+}
 
 // let mongoUrl = "mongodb://";
 
@@ -10,23 +18,25 @@ const config = require('config');
 //     if (err) {
 //         console.log('Can not connect to mongodb');
 //     } else {
-        const cors = require('cors');
-        const bodyParser = require('body-parser');
-        const express = require('express');
-        const app = express();
-        const curveRouter = require('./server/router/curve.router');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const curveRouter = require('./server/router/curve.router');
+app.get('/', (req, res) => {
+	res.json({name: "wi-curve-transfer", serverId: serverId, version: "1.0"});
+});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(bodyParser.json());
 
-        app.use(bodyParser.urlencoded({ extended: false }));
-        app.use(cors());
-        app.use(bodyParser.json());
+app.use('/curve', curveRouter);
 
-        app.use('/curve', curveRouter);
+let port = process.env.PORT || config.get("port");
 
-        let port = process.env.PORT || config.get("port");
-        
-        app.listen(port, () => {
-            console.log('App start listen', port);
-        });
+app.listen(port, () => {
+	console.log('App start listen', port);
+});
 //     }
 // });
 
